da7f33db05be007858295997ecbf5fbc40abadeb
ivofoppa <ivo.foppa@gmail.com>
2018-09-28 10:33:14 -0400

    Adjusting for vacc. fixed

--- WIcode/SIR stochastic discrete with unimodal vacc adj.R
+++ WIcode/SIR stochastic discrete with unimodal vacc adj.R
@@ -21,7 +21,7 @@ seas <- 500 ## Number of days in epidemic
 vacc1 <- 0.47
 
 prevdur <- 100 ## vaccination before transmission
-vdur <- prevdur + 150 ### duration of vaccination
+vdur <- prevdur + 200 ### duration of vaccination
 
 vdurls <- 1:vdur
 
@@ -68,6 +68,7 @@ while (time <= prevdur){
 }
 
 studydata <- array(0,dim = c(seas,1 + 2 * nvaccat + 2))
+studydata2 <- array(0,dim = c(seas,1 + 2 * nvaccat + 2))
 
 infnum <- 100
 
@@ -90,6 +91,8 @@ while (time <= seas + prevdur){
   newinfnv <- rbinom(1, Snv, pnvinf)
   newinfv <- sapply(Sv, function(sv) rbinom(1, sv, pvinf))
   
+  newinfv2 <- sapply(seq_along(newinfv), function(k) ifelse(k <= (time - prevdur), 0, newinfv[k]))
+  
   newremnv <- rbinom(1,Inv,1 - exp(-delta))
   newremv <- sapply(Iv, function(iv) rbinom(1,iv, 1 - exp(-delta)))
 
@@ -108,16 +111,17 @@ while (time <= seas + prevdur){
   ###################################################################################################
   ### "Study" is conducted ##########################################################################
   ###################################################################################################
-  newinfv2 <- sapply(seq_along(newinfnv), function(k) ifelse(k <= (time - prevdur), 0, newinfv[k]))
-  
   casesls <- c(newinfnv,sapply(seq_along(vaccdelim[-1]), function(x) sum(newinfv[vaccdelim[x] : vaccdelim[x + 1]])))
+  casesls2 <- c(newinfnv,sapply(seq_along(vaccdelim[-1]), function(x) sum(newinfv2[vaccdelim[x] : vaccdelim[x + 1]])))
   
   controlsvsum <- Sv + Rv + Iv
   controlsvsum2 <- sapply(seq_along(controlsvsum), function(k) ifelse(k <= (time - prevdur), 0, controlsvsum[k]))
 
   controlsls <- c(Snv + Rnv + Inv,sapply(seq_along(vaccdelim[-1]), function(x) sum(controlsvsum[vaccdelim[x] : vaccdelim[x + 1]])))
+  controlsls2 <- c(Snv + Rnv + Inv,sapply(seq_along(vaccdelim[-1]), function(x) sum(controlsvsum2[vaccdelim[x] : vaccdelim[x + 1]])))
   
   studydata[time - prevdur,] <- c(time - prevdur,casesls,controlsls)
+  studydata2[time - prevdur,] <- c(time - prevdur,casesls2,controlsls2)
   ###################################################################################################
   ### Stop when no more transmission 
   if (sum(Iv)==0 & Inv == 0){
@@ -143,10 +147,11 @@ while (time <= seas + prevdur){
 delind <- which(rowSums(studydata)==0 | any(is.na(studydata)))
 
 studydata <- studydata[-delind,]
+studydata2 <- studydata2[-delind,]
 
 seas2 <- head(delind,1) - 1
 ### Reorganizing data set for analysis--only vaccinated
-dataset <- NULL
+dataset <- dataset2 <- NULL
 
 for (t in 1:seas2){
   totcases <- sum(studydata[t,2:7])
@@ -154,17 +159,32 @@ for (t in 1:seas2){
   oddscontrol <- totcases/totnoncases * ccratio
   pcontrol <- oddscontrol * totnoncases/Ntot 
   
+  totcases2 <- sum(studydata2[t,2:7])
+  totnoncases2 <- sum(studydata2[t,8:13])
+  oddscontrol2 <- totcases2/totnoncases2 * ccratio
+  pcontrol2 <- oddscontrol2 * totnoncases2/Ntot 
+  
   for (k in seq_along(vaccdelim[-1])){
     ncases <- studydata[t,2 + k]
     nnoncases <- studydata[t,8 + k]
     
+    ncases2 <- studydata2[t,2 + k]
+    nnoncases2 <- studydata2[t,8 + k]
+    
     ncontrols <- rbinom(1,nnoncases,pcontrol)
+    ncontrols2 <- rbinom(1,nnoncases2,pcontrol2)
     # ncontrols <- nnoncases
     if ((ncases > 0 & ncontrols > 0) & (!is.na(ncases) & !is.na(ncontrols))){
       datarec <- c(t,k,1,ncases)
       dataset <- rbind(dataset,datarec,deparse.level = 0)
       datarec <- c(t,k,0,ncontrols)
       dataset <- rbind(dataset,datarec,deparse.level = 0)
+    }
+    if ((ncases2 > 0 & ncontrols2 > 0) & (!is.na(ncases2) & !is.na(ncontrols2))){
+      datarec2 <- c(t,k,1,ncases2)
+      dataset2 <- rbind(dataset2,datarec2,deparse.level = 0)
+      datarec2 <- c(t,k,0,ncontrols2)
+      dataset2 <- rbind(dataset2,datarec2,deparse.level = 0)
     }
   }
 }
@@ -173,14 +193,21 @@ colnames(dataset) <- c('time','sincevacc','case','count')
 dataset <- data.frame(dataset)
 dataset$sincevacc <- factor(dataset$sincevacc)
 
+colnames(dataset2) <- c('time','sincevacc','case','count')
+dataset2 <- data.frame(dataset2)
+dataset2$sincevacc <- factor(dataset2$sincevacc)
+
 # delind <- which(dataset$case==1 & dataset$count==0)
 # dataset <- dataset[-delind,]
 
 cond_logist <- clogit(case ~ sincevacc + strata(time),weights = count, data = dataset,method = 'approximate')
 summary(cond_logist)
 
-filepath <- paste0('C:/Users/IFoppa/Documents/GitHub/Waning-Immunity-artefact/WIwriteup/WIplots/simul_',prevdur,'_',vdur,'.RData')
-save(dataset,studydata,file = filepath)
+cond_logist2 <- clogit(case ~ sincevacc + strata(time),weights = count, data = dataset2,method = 'approximate')
+summary(cond_logist2)
+
+filepath <- paste0('C:/Users/VOR1/Documents/GitHub/Waning-Immunity-artefact/WIwriteup/WIplots/simul_adj_',prevdur,'_',vdur,'.RData')
+save(dataset,dataset2,studydata,studydata2,file = filepath)
 
 logist <- glm(case ~ sincevacc ,weights = count, data = dataset,family = binomial(link = 'logit'))
 summary(logist)
