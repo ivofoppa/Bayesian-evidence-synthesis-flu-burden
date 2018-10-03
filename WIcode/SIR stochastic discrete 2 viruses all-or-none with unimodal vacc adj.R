c55ae493704378ba9c6d82610351cd41fe570624
ivofoppa <ivo.foppa@gmail.com>
2018-10-02 20:08:13 -0400

    Latest modifications

--- WIcode/SIR stochastic discrete 2 viruses all-or-none with unimodal vacc adj.R
+++ WIcode/SIR stochastic discrete 2 viruses all-or-none with unimodal vacc adj.R
@@ -4,7 +4,7 @@ library(survival)
 Ntot <- 2000000
 
 r10 <- 1.5
-r20 <- 1.8
+r20 <- 1.6
 delta <- 1/4 ## infectious period
 
 prem <- 1 - exp(-delta) ## Daily removal probability
@@ -22,6 +22,11 @@ vacc1 <- 0.47 ## cumulative vaccination coverage
 vs1 <- .3 ## proportion of subject who, if vacc. remain susc. to virus 1
 vs2 <- .6 ## proportion of subject who, if vacc. remain to virus 2
 vs0 <- 1 - vs1 - vs2
+
+# ## scaling the second virus to about virus 1 transmissability
+# fac1 <- vacc1 *vs1 + (1 - vacc1)
+# fac2 <- vacc1 *vs2 + (1 - vacc1)
+# r20 <- r10 * fac2/fac1
 
 prevdur <- 100 ## vaccination before transmission
 vdur <- prevdur + 200 ### duration of vaccination
