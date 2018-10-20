7c3efa17d943014af6d2820810e088124171a782
ivofoppa <ivo.foppa@gmail.com>
2018-10-20 08:49:46 -0400

    Update WIwriteup/integral check.R

--- WIwriteup/integral check.R
+++ WIwriteup/integral check.R
@@ -7,6 +7,10 @@ Npop <- 2e+6 ### Source population
 vacc <- .5   ### Vaccination rate
 AR <- .2     ### Seasonal attack rate
 seas <- 150  ### Duration of season in days
+delta <- .01 ### size of the time step (in days)
+
+time <- seq(0,seas,delta)
+
 lambda <- -log(AR)/seas ### Daily infection rate (per person per day)
 rho <- 0.01  ### Rate of vaccination protection loss
 R0 <- .3     ### Initial vaccine failure (proportion)
@@ -26,21 +30,44 @@ R <- R0             ### Imnitialize vaccine failures over time
 ###################################################################################################
 ###################################################################################################
 ###################################################################################################
-R <- sapply(2:seas, function(d) R[d] <<- 1 - (1 - R[d-1])*exp(-rho));
-Rprime <- diff(R)
+R <- sapply(seq_along(time)[-1], function(d) R[d] <<- 1 - (1 - R[d-1])*exp(-rho*delta));
+Rprime <- diff(R)/delta
 
 surv_a <- function(a,u){
-  Rprime[u]*exp(-lambda*(a - u))
+  if (!(u %in% time) | !(a %in% time)){
+    stop('The time argument have to be contained in the "time" object!')
+  }
+  Rpr <- Rprime[which(time==u)-1]
+  Rpr*exp(-lambda*(a - u))
 }
 
-a <- 40
-ls <- sapply(seq_along(Rprime[1:a]),function(u) surv_a(a,u))
+a <- 30
+Fa <- sapply(time[2:which(time==a)],function(u) surv_a(a,u))
+c(sum(Fa*delta),rho*(1 - R0)/(lambda - rho) * (exp(-rho*a) - exp(-lambda*a)))
+
+Fals <- NULL
+
+for (a in time[-1]){
+  Fa <- sapply(time[2:which(time==a)],function(u) surv_a(a,u))
+  Fals <- rbind(Fals,c(sum(Fa*delta),rho*(1 - R0)/(lambda - rho) * (exp(-rho*a) - exp(-lambda*a))),deparse.level = 0)
+  if (a %in% 1:seas){
+    cat(paste0('Up to age ',a,' done!\n'))
+  }
+}
+
+Faprime <- diff(Fals[,1])/delta
+
+a <- 30
+
+rho*(1 - R0)/(lambda - rho) * (-rho*exp(-rho*a) + lambda*exp(-lambda*a))
+Faprime[which(time==a)-1]
 ###################################################################################################
 ###################################################################################################
+Sprimea <- -Rprime[which(time==a) - 1] - lambda*R0*exp(-a*lambda) - rho*(1 - R0)/(lambda - rho) * (-rho*exp(-rho*a) + lambda*exp(-lambda*a))
 
+Sa <- 1 - R[which(time==a)] + R0*exp(-a*lambda) + rho*(1 - R0)/(lambda - rho) * (exp(-rho*a) - exp(-lambda*a))
 
-
-
+-Sprimea/Sa
 
 
 
