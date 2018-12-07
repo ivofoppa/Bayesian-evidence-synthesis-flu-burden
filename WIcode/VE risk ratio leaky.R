56ef8489a1487952229128fd0d11f754da0500e8
ivofoppa <ivo.foppa@gmail.com>
2018-12-07 14:44:36 -0500

    Update VE risk ratio leaky.R

--- WIcode/VE risk ratio leaky.R
+++ WIcode/VE risk ratio leaky.R
@@ -28,13 +28,13 @@ Ntot <- 2000000
 
 pSI <- function(r0,Iv,Inv){ ## infection probability
   beta <- r0 * delta
-  if((beta * (Iv + I2))==0){
+  if((beta * (Iv + Inv))==0){
     return (c(0,0,1))
   } else {
     lambda <- (beta * (Iv + Inv)) / Ntot
     pinfv <- as.numeric(1-exp(-lambda * phi))
     pinfnv <- as.numeric(1-exp(-lambda))
-    return (c(pinfv,pinfnv,1 - pinfv,1 - pinfnv))
+    return (c(pinfv,pinfnv))
   }
 }
 
@@ -64,108 +64,73 @@ Rv <- as.integer(inits$Rvinit)
 
 time <- 1
 
-## Infections in unvaccinated
-pinfnv <-  S1nv/(S1nv + S2nv + S0nv + S12nv + sum(S1v) + sum(S12v)) ### probability that infections with virus 1 are in type 1 individuals
-pinf21nv <-  S2nv/(S1nv + S2nv + S0nv + S12nv + sum(S1v) + sum(S12v))  ### probability that infections with virus 1 are in type 2 individuals
-pinf01nv <-  S0nv/(S1nv + S2nv + S0nv + S12nv + sum(S1v) + sum(S12v))  ### probability that infections with virus 1 are in type 0 individuals
-pinf121nv <-  S12nv/(S1nv + S2nv + S0nv + S12nv + sum(S1v) + sum(S12v))  ### probability that infections with virus 1 are in type 12 individuals
-## virus 2
-pinf12nv <-  S1nv/(S1nv + S2nv + S0nv + S12nv + sum(S2v) + sum(S12v)) ### probability that infections with virus 1 are in type 1 individuals
-pinf22nv <-  S2nv/(S1nv + S2nv + S0nv + S12nv + sum(S2v) + sum(S12v))  ### probability that infections with virus 1 are in type 2 individuals
-pinf02nv <-  S0nv/(S1nv + S2nv + S0nv + S12nv + sum(S2v) + sum(S12v))  ### probability that infections with virus 1 are in type 0 individuals
-pinf122nv <-  S12nv/(S1nv + S2nv + S0nv + S12nv + sum(S2v) + sum(S12v))  ### probability that infections with virus 1 are in type 12 individuals
-## Infections in vaccinated
-pinf11v <-  sum(S1v)/(S1nv + S2nv + S0nv + S12nv + sum(S1v) + sum(S12v))
-pinf121v <-  1 - pinf11nv - pinf21nv - pinf01nv - pinf121nv - pinf11v  ### probability that infections with virus 1 are in type 1 individuals
-## virus 2
-pinf22v <-  sum(S2v)/(S1nv + S2nv + S0nv + S12nv + sum(S2v) + sum(S12v))
-pinf122v <-  1 - pinf12nv - pinf22nv - pinf02nv - pinf122nv - pinf22v  ### probability that infections with virus 1 are in type 1 individuals
-
-## infection numbers by virus and type
-## virus 1
-inf1 <- rmultinom(1,inf1num,c(pinf11nv,pinf21nv,pinf01nv,pinf121nv,pinf11v,pinf121v))
-inf11nv <- inf1[1]
-inf21nv <- inf1[2]
-inf01nv <- inf1[3]
-inf121nv <- inf1[4]
-inf11vnum <- inf1[5]
-inf11v <- c(rmultinom(1,inf11vnum,rep(1/prevdur,prevdur)),rep(0,seas))
-inf121vnum <- inf1[6]
-inf121v <- c(rmultinom(1,inf121vnum,rep(1/prevdur,prevdur)),rep(0,seas))
-
-## virus 2
-inf2 <- rmultinom(1,inf2num,c(pinf12nv,pinf22nv,pinf02nv,pinf122nv,pinf22v,pinf122v))
-inf12nv <- inf2[1]
-inf22nv <- inf2[2]
-inf02nv <- inf2[3]
-inf122nv <- inf2[4]
-inf22vnum <- inf2[5]
-inf22v <- c(rmultinom(1,inf22vnum,rep(1/prevdur,prevdur)),rep(0,seas))
-inf122vnum <- inf2[6]
-inf122v <- c(rmultinom(1,inf122vnum,rep(1/prevdur,prevdur)),rep(0,seas))
-
-#########################################################################################
-#########################################################################################
-## updating states: Infections with virus 1
-I1nv <- I1nv + inf11nv + inf21nv + inf01nv + inf121nv
-I1v <- I1v + inf11v + inf121v
-## updating states: Infections with virus 2
-I2nv <- I2nv + inf12nv + inf22nv + inf02nv + inf122nv
-I2v <- I2v + inf22v + inf122v
-
-## Updating # susceptibles
-## unvaccinated
-S1nv <- S1nv - inf11nv - inf12nv
-S2nv <- S2nv - inf21nv - inf22nv
-S0nv <- S0nv - inf01nv - inf02nv
-S12nv <- S12nv - inf121nv - inf122nv
-## vaccinated
-S1v <- S1v - inf11v
-S2v <- S2v - inf22v
-S12v <- S12v - inf121v - inf122v
-
-I1 <- sum(c(I1nv,I1v))
-I2 <- sum(c(I2nv,I2v))
-
-I1ls <- I2ls <- kls <- NULL
-
 ## Running model
-source('model run.R') ## run initialization code
+studydata1 <- studydata2 <- NULL
 
-## ... and setting-up data
-source('data set prep.R') ## run initialization code
+while (time <= seas){
+  pinf <- pSI(r0,Iv,Inv) ## for fully susceptible
+  ## unvaccinated
+  infv <- rbinom(1, Sv, pinf[1])
+  ## vaccinated
+  infnv <- rbinom(1, Snv, pinf[2])
+
+  ### updating susceptibles
+  Sv <- Sv - infv
+  Snv <- Snv - infnv
+  ## updating other states: Infections
+  Iv <- Iv + infv
+  Inv <- Inv + infnv
+
+  remv <- rbinom(1,Iv, prem)
+  remnv <- rbinom(1,Inv, prem)
+  ## numbers removed from infectous
+  Iv <- Iv - remv
+  Inv <- Inv - remnv
+  ## numbers removed from virus infectious
+  Rv <- Rv + remv
+  Rnv <- Rnv + remnv
+  
+  ### Interrupt evaluation if no more transmission
+  if (Iv==0 & Inv==0 | is.na(Iv) | is.na(Inv)){
+    break()
+  }
+  ###################################################################################################
+  ### "Study" is conducted ##########################################################################
+  ###################################################################################################
+  ncases <- infv + infnv
+  ncontrols <- ccratio*ncases
+  ncontrolel <- Ntot - Iv - Inv
+  
+  sratio <- ncontrols/ncontrolel
+  controlsv1 <- rbinom(1,Sv + Rv,sratio)
+  controlsnv1 <- rbinom(1,Snv + Rnv,sratio)
+
+  controlsv2 <- rbinom(1,Svinit,sratio)
+  controlsnv2 <- rbinom(1,Snvinit,sratio)
+  
+  datarr1 <- rbind(c(time,1,1,infv),c(time,1,0,infnv),c(time,0,1,controlsv1),c(time,0,0,controlsnv1),deparse.level = 0)
+  datarr2 <- rbind(c(time,1,1,infv),c(time,1,0,infnv),c(time,0,1,controlsv2),c(time,0,0,controlsnv2),deparse.level = 0)
+  
+  studydata1 <- rbind(studydata1,datarr1,deparse.level = 0)
+  studydata2 <- rbind(studydata2,datarr2,deparse.level = 0)
+  ###################################################################################################
+  ###################################################################################################
+  ### Stop when no more transmission 
+  time <- time + 1
+}
+
+studydata1 <- data.frame(studydata1)
+studydata2 <- data.frame(studydata2)
+colnames(studydata1) <- colnames(studydata2) <- c('day','case','vacc','count')
 ######################################################################################################
 ######################################################################################################
-plot(I1ls, ylim = c(0,max(c(I1ls,I2ls))),type = 'l',col = 'blue')
-lines(I2ls,col = 'red')
 ######################################################################################################
-######################################################################################################
-# cond_logist <- clogit(case ~ sincevacc + strata(time),weights = count, data = dataset,method = 'approximate')
-# summary(cond_logist)
-# 
-cond_logist1 <- clogit(case ~ sincevacc + strata(time),weights = count, data = dataset1,method = 'approximate')
-summary(cond_logist1)
+logist1 <- glm(case ~ vacc,weights = count, data = studydata1,family = binomial())
+logist2 <- glm(case ~ vacc,weights = count, data = studydata2,family = binomial())
 
-cond_logist2 <- clogit(case ~ sincevacc + strata(time),weights = count, data = dataset12,method = 'approximate')
-summary(cond_logist2)
+c1 <- sum(studydata0$count[which(studydata0$case==1 & studydata0$vacc==1)])
+c0 <- sum(studydata0$count[which(studydata0$case==1 & studydata0$vacc==0)])
 
-cond_logist3 <- clogit(case ~ sincevacc + strata(time),weights = count, data = dataset3,method = 'approximate')
-summary(cond_logist3) ## sincevacc not a factor
-
-logist1 <- glm(case ~ sincevacc,weights = count, data = dataset,family = binomial(link = 'logit'))
-summary(logist1)
-
-logist2 <- glm(cbind(cases,controls) ~ vacc + late + late*vacc, data = dataset4,family = binomial(link = 'logit'))
-summary(logist2)
-
-logist2 <- glm(cbind(cases,controls) ~ vacc + late + late*vacc, data = dataset3,family = binomial(link = 'logit'))
-summary(logist2)
-######################################################################################################
-######################################################################################################
-
-plot(VEls[1:seas2])
-lines(trueVEls[1:seas2],col = 'red')
-
-filepath <- paste0('C:/Users/VOR1/Documents/GitHub/Waning-Immunity-artefact/WIwriteup/WIplots/simul_adj_',prevdur,'_',vdur,'.RData')
-save(dataset,dataset2,studydata,studydata2,file = filepath)
-
+1 - c1/Svinit/(c0/Snvinit)
+1-exp(logist1$coefficients[2])
+1-exp(logist2$coefficients[2])
